f4714a4f59409e6ef9ba5bc1cab670778f970297
iktushar01 <iktushar2020@gmail.com>
2026-06-27 10:35:43 +0600

    feat: update CORS configuration to include localhost:5173 for development

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.set("views",path.resolve(process.cwd(), `src/app/templates`) )
 
 const corsOptions = {
-    origin: [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
+    origin: [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5173", "http://localhost:5000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
